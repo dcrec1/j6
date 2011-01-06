fe681f5e4357086d774a9f92df8161d7c53cdcc4
Diego Carrion <dc.rec1@gmail.com>
2011-01-06 21:00:04 -0200

    hide the relative label when focusing a swapable input

--- src/j6.js
+++ src/j6.js
@@ -1,29 +1,5 @@
-;(function($) {
-  
-  $.j6 = {
-		VERSION: "0.0.1",
-		defaults: {
-			key: 'value'
-		}
-	};
-
-	$.fn.extend({	  
-	  j6: function(settings) {   
-      settings = $.extend({}, $.j6.defaults, settings);    
-      return this.each( function(){
-        self = this;
-        
-        // NOTE: semicolons are needed more if you are planning on packing your code
-        
-        // console.log(this);
-        //  console.log($(this));
-        //  console.log($.j6.VERSION);
-        //  console.log($.j6.defaults.key);
-        
-        // your plugin
-        
-      })
-		}
-	  
-	})
-})(jQuery);
+$(function($) {
+  $("li.swapable input").focus(function() {
+     $(this).siblings("label").hide(); 
+  });
+});
